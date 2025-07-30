bb93c8b4ffa15124ea77f4dbedd65b9b94bfe244
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-07-30 13:08:59 +0700

    selesai

---
+++ get-post/admin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1>selamat datang <?= $_POST["nama"]; ?>!!</h1> -->
+    <h2>Selamat datang admin</h2>
+</body>
+</html>
